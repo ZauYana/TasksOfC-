a5dc3bc80b26ef0f8718146299dbb0d0db7f0722
ZauYana <zaulskayana@gmail.com>
2022-01-29 16:38:01 +0300

    well done

---
+++ Exm69/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти сумму элементов от M до N
+
+
+void sumNumbers(int a, int b)
+{
+
+    if (a <= b)
+    {
+        Console.WriteLine(a);
+        sumNumbers(a + 1, b);
+    }
+}
+
+int sumOfNum (int a, int b)
+{
+    if (a<=b) return a+sumOfNum(a+1,b);
+    return 0;
+
+}
+int M = 10;
+int N = 27;
+sumNumbers(M, N);
+int result = sumOfNum(M,N);
+Console.WriteLine(result);
